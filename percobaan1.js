2299874a2de7c741a52b077e4ec36c9ebf8bf806
Adityo Arya Putra <adityo.arya.ft21@mail.umy.ac.id>
2023-06-18 09:49:53 +0700

    Finish

---
+++ percobaan1.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video(){
+    alert("Ini Video ALAM");
+});
+
+audio.addEventListener("click", function Audio(){
+    alert("Ini Lagu ALAM")
+});
+
+gambar.addEventListener("click", function Image(){
+    alert("Ini Gambar ALAM");
+});
